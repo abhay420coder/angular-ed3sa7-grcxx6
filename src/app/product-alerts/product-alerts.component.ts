c3ccf673f2499f60a3ba1e620e2b17ee094e2468
abhay420coder <ak8294836065@gmail.com>
2022-09-02 12:59:09 +0530

    5 files modified, 4 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -11,7 +11,7 @@
 //   ngOnInit(): void {}
 // }
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -20,5 +20,6 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
-  @Input() product!: Product;
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
